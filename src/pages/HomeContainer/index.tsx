0ea1f3ea188886e3d910f212be0cda7f268bc4c7
yanlele <331393627@qq.com>
2019-10-13 22:44:13 +0800

    feat: 解决报错问题

--- src/pages/HomeContainer/index.tsx
+++ src/pages/HomeContainer/index.tsx
@@ -1,9 +1,17 @@
 import React, { FC } from 'react';
+import styles from './index.less';
 
 const HomeContainer: FC = () => {
   return (
     <>
       <h2>爬虫数据</h2>
+      <div className={styles.container}>
+        内容
+
+        <div className="content">
+          我要变色
+        </div>
+      </div>
     </>
   );
 };
